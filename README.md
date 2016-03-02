251a710d959865b61426234ad29270a51c16d9c7
Tobias Gulbrandsen Waaler <tobias.waaler@nrk.no>
2016-03-02 09:27:20 +0100

    project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+## Resources
+[Intellij Plugin Development Guidelines](https://www.jetbrains.com/idea/help/plugin-development-guidelines.html)
+[IntelliJ Platform SDK Documentation]http://www.jetbrains.org/intellij/sdk/docs/index.html)
---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+
+public class HelloWorld extends AnAction {
+
+	@Override
+	public void actionPerformed(AnActionEvent e) {
+		// TODO: insert action logic here
+	}
+}
